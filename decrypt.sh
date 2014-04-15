2f55eb14e259ec2a835df2a4667b2e2a59504556
Tyler Anderson <unlimitedbacon@gmail.com>
2014-04-15 15:34:20 -0700

    Stuff

---
+++ decrypt.sh
@@ -0,0 +1,21 @@
+for password in $(cat passwords.txt)
+do
+	#for cipher in $(openssl list-cipher-commands)
+	for cipher in $(cat ciphers.txt)
+	do
+		echo $cipher $password
+		openssl enc -d -$cipher -in 1.bin -out 1.bin.txt -pass pass:$password
+		success=$?
+		if [ $success == 0 ]; then
+			echo "SUCCESS!"
+			for file in $(ls *.bin)
+			do
+				openssl enc -d -$cipher -in $file -out $out.txt -pass pass:$password
+			done
+			break
+		fi
+	done
+	if [ $success == 0 ]; then
+		break
+	fi
+done
